7c39cfa3cbe4aceebc54f1eedf436881c6422d31
nomad5679 <93387966+nomad5679@users.noreply.github.com>
2022-06-30 16:19:48 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import time
+
+while True:
+    print ("""
+localtime.h:17: error: 'axesParam1' has not been declared
+localtime.h:18: error: 'mainWin' has not been declared
+localtime.h:26: error: ISO C++ forbids declaration of 'axesParam1' with no type
+localtime.h:26: error: expected ';' before '*' token
+localtime.h:27: error: ISO C++ forbids declaration of 'mainWin' with no type
+localtime.h:27: error: expected ';' before '*' token
+In file included from trackinputstatus.h:5,
+from trackinput.h:5,
+i found amogus
+from mainwin.h:8,
+from geoparam.h:5,
+from axesparam3.h:5,
+from axesparam2.h:5,
+from axesparam1.h:5,
+from main.cpp:14:
+trackparamstatus.h:16: error: 'mainWin' has not been declared
+trackparamstatus.h:24: error: ISO C++ forbids declaration of 'mainWin' with no type
+trackparamstatus.h:24: error: expected ';' before '*' token
+main.cpp: In function 'int main(int, char**)':
+main.cpp:52: error: no matching function for call to 'localTime::setChildren(axesParam1*)'
+localtime.h:17: note: candidates are: void localTime::setChildren(int*)
+main.cpp:61: error: no matching function for call to 'localTime::setHome(mainWin*)'
+localtime.h:18: note: candidates are: void localTime::setHome(int*)
+main.cpp:62: error: no matching function for call to 'trackParamStatus::setHome(mainWin*)'
+trackparamstatus.h:16: note: candidates are: void trackParamStatus::setHome(int*)
+""")
+    time.sleep(1)
+    print ("amogus lost... error 506")
+    time.sleep(1)
+    print("searching again")
+    time.sleep(0.5)
